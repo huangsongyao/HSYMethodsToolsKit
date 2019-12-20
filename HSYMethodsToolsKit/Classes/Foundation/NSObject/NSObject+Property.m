d4b6762d4cb7fca3ac34a5dd275a5a99ad4f4240
huangsongyao <317398895@qq.com>
2019-12-20 10:47:12 +0800

    commit -> HSYMethodsToolsKit

--- HSYMethodsToolsKit/Classes/Foundation/NSObject/NSObject+Property.m
+++ HSYMethodsToolsKit/Classes/Foundation/NSObject/NSObject+Property.m
@@ -23,7 +23,7 @@
 
 - (id)hsy_getPropertyForKey:(NSString *)key
 {
-    return objc_getAssociatedObject(self, &key);
+    return objc_getAssociatedObject(self, CFBridgingRetain(key));
 }
 
 - (void)hsy_setProperty:(id)property
@@ -32,7 +32,7 @@
 {
     [self willChangeValueForKey:key];
     objc_AssociationPolicy objcAssociationPolicy = [self.class toObjcAssociationPolicy:associationPolicy];
-    objc_setAssociatedObject(self, &key, property, objcAssociationPolicy); 
+    objc_setAssociatedObject(self, CFBridgingRetain(key), property, objcAssociationPolicy);
     [self didChangeValueForKey:key];
 }
 
